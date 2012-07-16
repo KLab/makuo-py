90f5a6a58614c27f2408cddd38e47e96d6c86efd
INADA Naoki <songofacandy@gmail.com>
2012-07-16 20:54:42 +0900

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='makuo',
+      version='0.1',
+      description='makuosan client',
+      author='INADA Naoki',
+      author_email='songofacandy@gmail.com',
+      url='https://github.com/KLab/makuo-py',
+      py_modules='makuo.py',
+      )
